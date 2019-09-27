606ec6b40a1114c30aedbec989ae2e540e08b2ef
Jeff Swisher <jefftswisher@gmail.com>
2019-09-27 20:38:44 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,3 +1,62 @@
 def first_challenge
-   epic_tragedy = {}
+   epic_tragedy = {
+     :montagues => {
+       :patriarch => {
+         :name => "Lord Montague",
+         :age => "53"
+       },
+       :matriarch => {
+         :name => "Lady Montague",
+         :age => "54"
+       }
+       :hero => {
+         :name => "Romeo",
+         :age => "15",
+         :status => "Alive"
+       }
+
+    :heros_friends => {
+      :friend_1 => {
+        :name => "Benvolio",
+        :age => "17",
+        :attitude => "worried"
+      }
+      :friend_2 => {
+        :name => "Mercutio",
+        :age => "18",
+        :attitude => "hot-headed"
+      }
+    }
+       
+     },
+
+     :capulet => {
+       :patriarch => {
+         :name => "Lord capulet",
+         :age => "50"
+       }
+       :matriarch => {
+         :name => "Lady Capulet",
+         :age => "51"
+       }
+       :heroine => {
+         :name => "Juliet",
+         :age => "15"
+         :status => "Alive"
+       }
+
+       :heroines_friends => {
+         :friend_1 => {
+           :name => "Steven",
+           :age => "30",
+           :attitude => "confused"
+         }
+         :friend_2 => {
+           :name => "Nurse",
+           :age => "44",
+           :attitude => "worried"
+         }
+       }
+     }
+   }
 end
