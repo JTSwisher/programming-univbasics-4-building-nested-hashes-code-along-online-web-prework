1a4ff941a0d6d2894655e1260490b27be20fb332
Jeff Swisher <jefftswisher@gmail.com>
2019-09-27 20:41:28 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -8,37 +8,36 @@ def first_challenge
        :matriarch => {
          :name => "Lady Montague",
          :age => "54"
-       }
+       },
        :hero => {
          :name => "Romeo",
          :age => "15",
          :status => "Alive"
-       }
+       },
 
     :heros_friends => {
       :friend_1 => {
         :name => "Benvolio",
         :age => "17",
         :attitude => "worried"
-      }
+      },
       :friend_2 => {
         :name => "Mercutio",
         :age => "18",
         :attitude => "hot-headed"
       }
     }
-       
-     },
+  },
 
      :capulet => {
        :patriarch => {
          :name => "Lord capulet",
          :age => "50"
-       }
+       },
        :matriarch => {
          :name => "Lady Capulet",
          :age => "51"
-       }
+       },
        :heroine => {
          :name => "Juliet",
          :age => "15"
@@ -50,7 +49,7 @@ def first_challenge
            :name => "Steven",
            :age => "30",
            :attitude => "confused"
-         }
+         },
          :friend_2 => {
            :name => "Nurse",
            :age => "44",
